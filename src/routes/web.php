307d2d585d5236890747742501314406cc1d1ec0
LazyAlpine <gungamer101@hotmail.com>
2023-03-04 09:18:48 +0000

    auth setup

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\VehicleAllocationController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Auth::routes();
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/vehicle/allocation', [VehicleAllocationController::class, 'show']);
